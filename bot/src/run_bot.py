037d4c7cd1af12f703aac40a3700e178fd0dc9eb
Emilio <emilio@cafenun.es>
2019-12-11 20:23:17 +0100

    Better sync between multiple bot instance

--- bot/src/run_bot.py
+++ bot/src/run_bot.py
@@ -2,6 +2,7 @@ import requests
 import json
 import base64
 import time
+import random
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options  
@@ -12,8 +13,6 @@ base_url = 'http://web:80'
 # Load error image
 error_image = base64.b64encode(open("error_image.png", "rb").read()).decode('utf-8')
 
-
-
 while True:
     try:
         # List pages to process
@@ -22,62 +21,70 @@ while True:
     except:
         print("Error while getting page list, server not ready yet?")
         time.sleep(10)
+        continue
 
-    for page in pages:
-        page_id = int(page['id'])
-        user_id = int(page['user_id'])
+    # Better synchronization between multiple bot instances
+    # 1. Random wait
+    time.sleep(random.randrange(1, 6))
 
-        print("Processing page %d from user %d" % (page_id, user_id))
-        
-        # Get a session for this user
-        res = requests.get('%s/bot/login_as_bot.php?backdoor=%s&user_id=%d' % (base_url, backdoor_pwd, user_id))
-        session = json.loads(res.text)['PHPSESSID']
-        print("Got session: %s" % session)
+    # 2. Pick a random page to process
+    if len(pages) == 0:
+        continue
 
-        dummy_page = '%s/empty.php' % (base_url)
-        page_url = '%s/page.php?id=%d' % (base_url, page_id)
-        cookies = 'PHPSESSID=%s;' % (session)
+    page = random.choice(pages)
 
-        image = error_image
+    page_id = int(page['id'])
+    user_id = int(page['user_id'])
 
-        # Browser setup
-        chrome_options = Options()
-        chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument("--headless")
-        chrome_options.add_argument("--disable-gpu")
-        chrome_options.add_argument("--disable-dev-shm-using")
-        chrome_options.add_argument("--disable-software-rasterizer")
-
-        driver = webdriver.Chrome(options=chrome_options)
-        driver.set_page_load_timeout(2)
-
-        # Safe handling
-        try:
-            # Inject cookies
-            driver.get(dummy_page)
-            driver.add_cookie({'name': 'PHPSESSID', 'value': session})
-
-            # Visit page
-            driver.get(page_url)
-
-            # Accept alert if present
-            try:
-                alert = driver.switch_to.alert
-                alert.accept()
-            except:
-                pass
-
-            image = driver.get_screenshot_as_base64()
-        except Exception as e:
-            print("Exception while taking screenshot: ")
-            print(e)
-        finally:
-            driver.close()
-
-        # Upload image or error
-        data = {'page_id': page_id, 'image': 'data:image/png;base64,%s' % image}
-        res = requests.post('%s/bot/upload_image.php?backdoor=%s' % (base_url, backdoor_pwd), data=data)
-
-        print("Upload status: %s" % res.text)
+    print("Processing page %d from user %d" % (page_id, user_id))
     
-    time.sleep(1)
+    # Get a session for this user
+    res = requests.get('%s/bot/login_as_bot.php?backdoor=%s&user_id=%d' % (base_url, backdoor_pwd, user_id))
+    session = json.loads(res.text)['PHPSESSID']
+    print("Got session: %s" % session)
+
+    dummy_page = '%s/empty.php' % (base_url)
+    page_url = '%s/page.php?id=%d' % (base_url, page_id)
+    cookies = 'PHPSESSID=%s;' % (session)
+
+    image = error_image
+
+    # Browser setup
+    chrome_options = Options()
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--disable-dev-shm-using")
+    chrome_options.add_argument("--disable-software-rasterizer")
+
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.set_page_load_timeout(2)
+
+    # Safe handling
+    try:
+        # Inject cookies
+        driver.get(dummy_page)
+        driver.add_cookie({'name': 'PHPSESSID', 'value': session})
+
+        # Visit page
+        driver.get(page_url)
+
+        # Accept alert if present
+        try:
+            alert = driver.switch_to.alert
+            alert.accept()
+        except:
+            pass
+
+        image = driver.get_screenshot_as_base64()
+    except Exception as e:
+        print("Exception while taking screenshot: ")
+        print(e)
+    finally:
+        driver.close()
+
+    # Upload image or error
+    data = {'page_id': page_id, 'image': 'data:image/png;base64,%s' % image}
+    res = requests.post('%s/bot/upload_image.php?backdoor=%s' % (base_url, backdoor_pwd), data=data)
+
+    print("Upload status: %s" % res.text)
